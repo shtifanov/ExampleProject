ac69cdfaf7869935b4da6912cb8d90cd5596f520
aleksandr.shtifanov <aleksandr.shtifanov@ibi-systems.de>
2019-02-27 20:30:46 +0100

    add new Data project with Person entity

---
+++ ExampleProject/ExampleProject.Data/Common/Address.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExampleProject.Data.Common
+{
+    [Owned]
+    public sealed class Address
+    {
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ ExampleProject/ExampleProject.Data/Enums/ColorEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace ExampleProject.Data.Enums
+{
+    public enum ColorEnum
+    {
+        Blue = 1,
+        Green = 2,
+        Purple = 3,
+        Red = 4,
+        Yellow = 5,
+        Turquoise = 6,
+        White = 7
+    }
+}
