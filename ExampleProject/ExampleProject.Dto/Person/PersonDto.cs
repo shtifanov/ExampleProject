c8d90934fc2bcca9c881f12f5e942c14a3564c91
aleksandr.shtifanov <aleksandr.shtifanov@ibi-systems.de>
2019-02-27 20:35:08 +0100

    add new Dto project

---
+++ ExampleProject/ExampleProject.Dto/Person/PersonDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ExampleProject.Dto.Person
+{
+    public sealed class PersonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Color { get; set; }
+    }
+}
