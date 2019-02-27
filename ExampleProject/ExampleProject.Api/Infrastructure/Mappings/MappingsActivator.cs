245874e61458297a8f42672e448d05c3816f008b
aleksandr.shtifanov <aleksandr.shtifanov@ibi-systems.de>
2019-02-27 20:35:48 +0100

    add automapper configuration for PersonDto

---
+++ ExampleProject/ExampleProject.Api/Infrastructure/Mappings/MappingsActivator.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ExampleProject.Api.Infrastructure.Mappings.Configurations;
+
+namespace ExampleProject.Api.Infrastructure.Mappings
+{
+    internal static class MappingsActivator
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(config =>
+            {
+                config.MapPerson();
+            });
+        }
+    }
+}
