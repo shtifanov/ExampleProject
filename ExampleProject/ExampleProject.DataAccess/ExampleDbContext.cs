75b0fbe507b36e4676b46175ff7c4b76f68bb689
aleksandr.shtifanov <aleksandr.shtifanov@ibi-systems.de>
2019-02-28 21:24:35 +0100

    move DbContext into another folder

--- ExampleProject/ExampleProject.DataAccess/ExampleDbContext.cs
+++ ExampleProject/ExampleProject.DataAccess/ExampleDbContext.cs
@@ -1,13 +1,12 @@
 ﻿using ExampleProject.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace ExampleProject.DataAccess.Context
+namespace ExampleProject.DataAccess
 {
     public sealed class ExampleDbContext : DbContext
     {
         public ExampleDbContext(DbContextOptions options) : base(options)
         {
-            
         }
 
         public DbSet<Person> Persons { get; set; }
