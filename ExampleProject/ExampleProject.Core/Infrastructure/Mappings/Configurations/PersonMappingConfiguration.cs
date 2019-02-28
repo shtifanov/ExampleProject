79fcd95761fc3a1c1f43afd0b6716216001861ed
aleksandr.shtifanov <aleksandr.shtifanov@ibi-systems.de>
2019-02-28 21:54:19 +0100

    add new PersonToCreateDto object and respective automapper configuration

--- ExampleProject/ExampleProject.Core/Infrastructure/Mappings/Configurations/PersonMappingConfiguration.cs
+++ ExampleProject/ExampleProject.Core/Infrastructure/Mappings/Configurations/PersonMappingConfiguration.cs
@@ -16,6 +16,12 @@ namespace ExampleProject.Core.Infrastructure.Mappings.Configurations
                 .ForMember(dto => dto.City, opt => opt.MapFrom(entity => entity.Address.City))
                 .ForMember(dto => dto.Color, opt => opt.MapFrom(entity => Enum.GetName(typeof(ColorEnum), entity.FavoriteColor).ToLowerInvariant()));
 
+            config.CreateMap<PersonToCreateDto, Data.Entities.Person>()
+                .ForMember(entity => entity.Surname, opt => opt.MapFrom(dto => dto.LastName))
+                .ForMember(entity => entity.FirstName, opt => opt.MapFrom(dto => dto.Name))
+                .ForPath(entity => entity.Address.PostalCode, opt => opt.MapFrom(dto => dto.ZipCode))
+                .ForPath(entity => entity.Address.City, opt => opt.MapFrom(dto => dto.City))
+                .ForMember(entity => entity.FavoriteColor, opt => opt.MapFrom(dto => Enum.Parse<ColorEnum>(dto.Color, true)));
             return config;
         }
     }
