529347a49f9082fde5ffebd85039a68e09df3672
aleksandr.shtifanov <aleksandr.shtifanov@ibi-systems.de>
2019-02-28 21:29:27 +0100

    add IRepository & IDb interfaces

---
+++ ExampleProject/ExampleProject.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExampleProject.DataAccess.Interfaces
+{
+    public interface IRepository<TEntity>
+        where TEntity : class
+    {
+        IQueryable<TEntity> Get();
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
